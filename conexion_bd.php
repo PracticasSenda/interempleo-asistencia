ee67aacfe87b3f96d71f556bee061c6f437713e8
PracticasSenda <practicas@sendagestion.com>
2025-10-07 12:43:00 +0200

    Correcion conexión

--- conexion_bd.php
+++ conexion_bd.php
@@ -6,8 +6,5 @@ $base_datos = "usuarios"; // Por ejemplo 'interempleo' o 'asistencias'
 
 $conexion = mysqli_connect($host, $usuario, $clave, $base_datos);
 
-if (!$conexion) {
-    die("❌ Error de conexión: " . mysqli_connect_error());
-}
-// echo "✅ Conexión correcta a la base de datos";
+
 ?>
