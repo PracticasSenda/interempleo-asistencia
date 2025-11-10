9bc9d583bf546895d96aade80ec20030385945c2
PracticasSenda <practicas@sendagestion.com>
2025-11-10 11:19:46 +0100

    cambios en la seguridad del archivo

--- config/db.php
+++ config/db.php
@@ -1,9 +1,20 @@
 <?php
-$host = "192.168.1.225";  // Servidor remoto
-$usuario = "root3";       // Usuario de la base de datos
-$clave = ""; // Aquí pon la contraseña real
-$base_datos = "usuarios"; // Por ejemplo 'interempleo' o 'asistencias'
+require __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+// Cargar el archivo credenciales.env desde la carpeta config
+$dotenv = Dotenv::createImmutable(__DIR__, ['credenciales.env']);
+
+$dotenv->load();
+
+// Leer las variables del entorno
+$host = $_ENV['DB_HOST'];
+$usuario = $_ENV['DB_USER'];
+$clave = $_ENV['DB_PASS'];
+$base_datos = $_ENV['DB_NAME'];
+
+// Crear la conexión
 $conexion = mysqli_connect($host, $usuario, $clave, $base_datos);
 
 $conexion->set_charset("utf8mb4");
